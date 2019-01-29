55ee3dd9151e50556811a7a49bcc8ed6ab826d03
Artem Zekov <mrstalon@MacBook-Air-Artem.local>
2019-01-29 10:02:32 +0300

    refactor: refactor HeaderContent

---
+++ client/views/main-page/components/video-slider/VideoSlider.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class VideoSlider extends React.Component {
+
+  render() { 
+    return (
+      <section className="video-slider">
+
+      </section>
+    )
+  }
+}
+
+export default VideoSlider
